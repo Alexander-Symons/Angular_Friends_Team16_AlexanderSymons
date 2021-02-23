f427764a2fa2fb3433688867ee8b7d72c84d43cc
robbejs <43341126+robbejs@users.noreply.github.com>
2021-02-23 10:20:47 +0100

    Food

---
+++ Ajax_Polling-master/Ajax_Polling-master/src/main/java/Consumptie.java
@@ -0,0 +1,46 @@
+public class Consumptie {
+
+    private String naam;
+    private String bescchrijving;
+    private String type;
+    private double prijs;
+
+    public Consumptie(String naam, String bescchrijving, String type, double prijs){
+        setNaam(naam);
+        setBescchrijving(bescchrijving);
+        setType(type);
+        setPrijs(prijs);
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getBescchrijving() {
+        return bescchrijving;
+    }
+
+    public void setBescchrijving(String bescchrijving) {
+        this.bescchrijving = bescchrijving;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(double prijs) {
+        this.prijs = prijs;
+    }
+}
---
+++ Ajax_Polling-master/Ajax_Polling-master/src/main/java/ConsumptieRepository.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsumptieRepository {
+
+    private ArrayList<Consumptie> consumptieList;
+
+    public ConsumptieRepository(){
+        this.consumptieList = new ArrayList<>();
+        Consumptie spaghetti = new Consumptie("spaghetti", "Italiaanse pasta met bolognaise saus", "Pasta", 7.50);
+        Consumptie pizza_special = new Consumptie("Pizza Special", "Een goed belegde pizza", "Pizza", 10.75);
+        Consumptie cola = new Consumptie("cola", "Een zoete drank", "Soft drink", 2.00);
+        Consumptie pizza_hawai = new Consumptie("Pizza hawa&#239;", "Een pizza met ham en ananas", "Pizza", 9.50);
+        consumptieList.add(spaghetti);
+        consumptieList.add(pizza_special);
+        consumptieList.add(cola);
+        consumptieList.add(pizza_hawai);
+    }
+
+    public void addConsumptie(Consumptie consumptie){
+        this.consumptieList.add(consumptie);
+    }
+
+    public ArrayList<Consumptie> getAllConsumpties(){
+        return consumptieList;
+    }
+}
