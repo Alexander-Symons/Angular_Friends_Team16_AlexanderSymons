1c0065dedbef59c87547eb2943174e73b17d798e
arnophpc <arnophpc@gmail.com>
2021-02-23 12:51:52 +0100

    Test

--- Ajax_Polling-master/Ajax_Polling-master/src/main/java/ConsumptieServlet.java
+++ Ajax_Polling-master/Ajax_Polling-master/src/main/java/ConsumptieServlet.java
@@ -14,6 +14,7 @@ public class ConsumptieServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private ConsumptieRepository consumptieRepository;
+    // test
 
     public ConsumptieServlet(){
         consumptieRepository = new ConsumptieRepository();
