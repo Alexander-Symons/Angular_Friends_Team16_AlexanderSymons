ab250d3a802915bd8d7d9f0ab635980005f48568
ArneArtois <43445014+ArneArtois@users.noreply.github.com>
2021-03-06 14:26:50 +0100

    Delete main.java

--- src/main/java_1b/main.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class main {
-    public static void main(String[] args) {
-        ConsumptieRepository repository = new ConsumptieRepository();
-        System.out.println(repository.getRandomConsumptie().getNaam());
-    }
-}
